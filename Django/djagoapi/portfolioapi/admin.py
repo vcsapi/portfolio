4fd3472299f06099e7c4e0f69c0b5d58978332d9
Vamer <noteb_7@mail.ru>
2025-09-05 22:54:14 +0500

    Release

--- Django/djagoapi/portfolioapi/admin.py
+++ Django/djagoapi/portfolioapi/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Portfolio
 
 # Register your models here.
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'type', 'users')
+    list_filter = ('status', 'type')
+    search_fields = ('name', 'description', 'tech')
+    ordering = ('-id',)
+    list_editable = ('status',)
